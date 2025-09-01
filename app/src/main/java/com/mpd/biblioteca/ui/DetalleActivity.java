0e3873b2dd5e7fc7790f14c7b540cd4d17797712
mpirizdutra89 <mpirizdutra@ulp.edu.ar>
2025-09-01 10:55:15 -0300

    diseño

--- app/src/main/java/com/mpd/biblioteca/ui/DetalleActivity.java
+++ app/src/main/java/com/mpd/biblioteca/ui/DetalleActivity.java
@@ -1,4 +1,38 @@
 package com.mpd.biblioteca.ui;
 
-public class DetalleActivity {
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.mpd.biblioteca.R;
+import com.mpd.biblioteca.databinding.ActivityDetalleBinding;
+import com.mpd.biblioteca.ui.viewModel.DetalleViewModel;
+
+public class DetalleActivity extends AppCompatActivity {
+    private ActivityDetalleBinding detalleBinding;
+    private DetalleViewModel detalleViewModel;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        /**
+        * Configuracion Binding
+        * */
+        detalleBinding=ActivityDetalleBinding.inflate(getLayoutInflater());
+        setContentView(detalleBinding.getRoot());
+
+        /**
+         *Configuracion ViewModel Detalle
+         * Anexamos la logica a la vista con observer y la llamada de metodos
+         * */
+        detalleViewModel=new ViewModelProvider(this).get(DetalleViewModel.class);
+
+        /**
+         * Observer
+         * **/
+    }
 }
